9a9868aae4deca394af0716fd936af718bac7455
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-16 15:00:50 +0900

    145-b

---
+++ ABC/-150/145/b.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+
+/*---------------------------------------------------------------------------------------------------
+　　　　　　　　　　　 ∧＿∧
+　　　　　 ∧＿∧ 　（´<_｀ ）　 Welcome to My Coding Space!
+　　　　 （ ´_ゝ`）　/　 ⌒i
+　　　　／　　　＼　 　  |　|
+　　　 /　　 /￣￣￣￣/　　|
+　 ＿_(__ﾆつ/　    ＿/ .| .|＿＿＿＿
+　 　　　＼/＿＿＿＿/　（u　⊃
+---------------------------------------------------------------------------------------------------*/
+int main() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  if (n%2 == 1) {
+    cout << "No" << endl;
+    return 0;
+  }
+  if (s.substr(0, n/2) == s.substr(n/2, n/2)) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+}
