960a15f0795d7445d7f824cba174d735c841575e
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-16 15:01:34 +0900

    206-b

---
+++ ABC/-210/206/b.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+
+/*---------------------------------------------------------------------------------------------------
+　　　　　　　　　　　 ∧＿∧
+　　　　　 ∧＿∧ 　（´<_｀ ）　 Welcome to My Coding Space!
+　　　　 （ ´_ゝ`）　/　 ⌒i
+　　　　／　　　＼　 　  |　|
+　　　 /　　 /￣￣￣￣/　　|
+　 ＿_(__ﾆつ/　    ＿/ .| .|＿＿＿＿
+　 　　　＼/＿＿＿＿/　（u　⊃
+---------------------------------------------------------------------------------------------------*/
+int main() {
+  int n;
+  cin >> n;
+
+  int ans = 1;
+  while(true) {
+    n-=ans;
+    ans++;
+    if(n<=0) break;
+  }
+  cout << ans - 1 << endl;
+}
