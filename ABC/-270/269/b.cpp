0e3b36649272cd8a901023e3034671e435d9dfc0
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-23 09:12:59 +0900

    269-b

---
+++ ABC/-270/269/b.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+
+/*---------------------------------------------------------------------------------------------------
+　　　　　　　　　　　 ∧＿∧
+　　　　　 ∧＿∧ 　（´<_｀ ）　 Welcome to My Coding Space!
+　　　　 （ ´_ゝ`）　/　 ⌒i
+　　　　／　　　＼　 　  |　|
+　　　 /　　 /￣￣￣￣/　　|
+　 ＿_(__ﾆつ/　    ＿/ .| .|＿＿＿＿
+　 　　　＼/＿＿＿＿/　（u　⊃
+---------------------------------------------------------------------------------------------------*/
+
+int main() {
+  vector<vector<char> > s(10, vector<char>(10));
+
+  rep(i, 0, 10) {
+    rep(j, 0, 10) {
+      cin >> s[i][j];
+    }
+  }
+  
+}
