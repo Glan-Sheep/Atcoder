3bde0185e83ebbe4feddb326392cc364e3b56dc1
Glan_Sheep <kopkcgungun@gmail.com>
2022-10-29 11:56:37 +0900

    212-c

--- ABC/-220/212/c.cpp
+++ ABC/-220/212/c.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 
 int main() {
-  int N;
-  cin >> N;
-  vector<int> vec(N);
-  for (int i = 0; i < N;i++) {
-    cin >> vec[i];
-  }
-  int count = 0;
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      if ((vec[i]-vec[j])%200 == 0) {
-        count++;
-      }
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(n), b(m);
+  for (int i = 0; i < n; i++) cin >> a[i];
+  for (int i = 0; i < m; i++) cin >> b[i];
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+  int ans = 1e9;
+  int count_n = 0;
+  int count_m = 0;
+  while((count_n < n) && (count_m < m)) {
+    ans = min(ans, abs(a[count_n] - b[count_m]));
+    if (a[count_n] > b[count_m]) {
+      count_m++;
+    } else {
+      count_n++;
     }
   }
-  cout << count << endl;
+  cout << ans << endl;
 }
