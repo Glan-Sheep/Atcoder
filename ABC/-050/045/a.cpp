b656183e22b4ddf4f299837b56c4dbbb924cf25c
Glan_Sheep <kopkcgungun@gmail.com>
2022-11-17 22:22:44 +0900

    045-a

---
+++ ABC/-050/045/a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+// type
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+// pair
+using pii = pair<int, int>;
+// vector
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using vpii = vector<pii>;
+
+/* define short */
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+
+/* func */
+inline int in_int() {int x; cin >> x; return x;}
+inline ll in_ll() {ll x; cin >> x; return x;}
+inline double in_double() {{double x; cin >> x; return x;}}
+inline string in_str() {string x; cin >> x; return x;}
+inline int ctoi(char c) {return c - '0';}
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << (a+b)*c / 2 << endl;
+}
