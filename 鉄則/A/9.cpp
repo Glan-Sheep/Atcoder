39785bf8a0ba1a5bf4290f3f2db08ba2d6224c6b
Glan_Sheep <kopkcgungun@gmail.com>
2022-11-21 20:32:39 +0900

    鉄則-9

--- 鉄則/A/9.cpp
+++ 鉄則/A/9.cpp
@@ -1,106 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// clang-format off
-/* accelration */
-// 高速バイナリ生成
-#pragma GCC target("avx")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
-// cin cout の結びつけ解除, stdioと同期しない(入出力非同期化)
-// cとstdの入出力を混在させるとバグるので注意
-struct Fast {Fast() {std::cin.tie(0); ios::sync_with_stdio(false);}} fast;
-
-/* alias */
-// type
-using ull = unsigned long long;
-using ll = long long;
-using ld = long double;
-// pair
-using pii = pair<int, int>;
-// vector
-using vi = vector<int>;
-using vl = vector<long>;
-using vll = vector<ll>;
-using vvi = vector<vi>;
-using vvl = vector<vl>;
-using vvll = vector<vll>;
-using vs = vector<string>;
-using vpii = vector<pii>;
-
-/* define short */
-#define pb push_back
-#define mp make_pair
-#define um unordered_map
-#define all(obj) (obj).begin(), (obj).end()
-#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
-#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
-#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
-
-/* REP macro */
-#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
-#define rep(i, n) reps(i, 0, n)
-#define rrep(i, n) reps(i, 1, n + 1)
-#define repd(i,n) for(ll i=n-1;i>=0;i--)
-#define rrepd(i,n) for(ll i=n;i>=1;i--)
-
-/* debug */
-// 標準エラー出力を含む提出はrejectされる場合もあるので注意
-#define debug(x) cerr << "\033[33m(line:" << __LINE__ << ") " << #x << ": " << x << "\033[m" << endl;
-
-/* func */
-inline int in_int() {int x; cin >> x; return x;}
-inline ll in_ll() {ll x; cin >> x; return x;}
-inline double in_double() {{double x; cin >> x; return x;}}
-inline string in_str() {string x; cin >> x; return x;}
-inline int ctoi(char c) {return c - '0';}
-// vector_finder: (arg)elementを vectorの先頭から(arg)search_lengthまで先頭から検索し、boolを返す
-// (arg)search_length: 走査するベクトル長の上限(先頭から何要素目までを検索対象とするか、1始まりで)
-template <typename T> inline bool vector_finder(std::vector<T> vec, T element, unsigned int search_length) {
-  auto itr = std::find(vec.begin(), vec.end(), element);
-  size_t index = std::distance( vec.begin(), itr );
-  if (index == vec.size() || index >= search_length) {return false;} else {return true;}
-}
-template <typename T> inline void print(const vector<T>& v, string s = " ")
-  {rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
-template <typename T, typename S> inline void print(const pair<T, S>& p)
-  {cout << p.first << " " << p.second << endl;}
-template <typename T> inline void print(const T& x) {cout << x << "\n";}
-template <typename T, typename S> inline void print(const vector<pair<T, S>>& v)
-  {for (auto&& p : v) print(p);}
-template <typename T, typename S> inline void print(const map<T, S>& m)
-  {for (auto&& p : m) print(p);}
-// 第一引数と第二引数を比較し、第一引数(a)をより大きい/小さい値に上書き
-template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
-template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
-// gcd lcm
-// clang-format on
+int H, W, N;
+int A[100009], B[100009], C[100009], D[100009];
+int X[1509][1509], Z[1509][1509];
 
 int main() {
-  int h, w, n;
-  cin >> h >> w >> n;
+	// 入力
+	cin >> H >> W >> N;
+	for (int t = 1; t <= N; t++) cin >> A[t] >> B[t] >> C[t] >> D[t];
 
-  vvi z(h, vi(w));
-  reps(i,0,n) {
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
-    z[a][b]++;
-    z[a][d+1]--;
-    z[c+1][b]--;
-    z[c+1][d+1]++;
-  }
-  reps(i,1,h+1) {
-    reps(j,1,w+1) {
-      z[i][j]+=z[i-1][j];
-    }
-  }
-  reps(i,1,h+1) {
-    reps(j,1,w+1) {
-      z[i][j]+=z[i-1][j];
-    }
-  }
-  rep(i,h) {
-    rep(j,w) cout << z[i][j] << " ";
-    cout << endl;
-  }
+	// 各日について加算
+	for (int t = 1; t <= N; t++) {
+		X[A[t]][B[t]] += 1;
+		X[A[t]][D[t] + 1] -= 1;
+		X[C[t] + 1][B[t]] -= 1;
+		X[C[t] + 1][D[t] + 1] += 1;
+	}
+
+	// 二次元累積和をとる
+	for (int i = 0; i <= H; i++) {
+		for (int j = 0; j <= W; j++) Z[i][j] = 0;
+	}
+	for (int i = 1; i <= H; i++) {
+		for (int j = 1; j <= W; j++) Z[i][j] = Z[i][j - 1] + X[i][j];
+	}
+	for (int j = 1; j <= W; j++) {
+		for (int i = 1; i <= H; i++) Z[i][j] = Z[i - 1][j] + Z[i][j];
+	}
+
+	// 出力
+	for (int i = 1; i <= H; i++) {
+		for (int j = 1; j <= W; j++) {
+			if (j >= 2) cout << " ";
+			cout << Z[i][j];
+		}
+		cout << endl;
+	}
+	return 0;
 }
