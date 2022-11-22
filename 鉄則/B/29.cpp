34addad1482503e90bf97d0b411b270aca148a66
Glan_Sheep <kopkcgungun@gmail.com>
2022-11-22 14:51:00 +0900

    鉄則B-29

---
+++ 鉄則/B/29.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+// type
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+// pair
+using pii = pair<int, int>;
+// vector
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using vpii = vector<pii>;
+
+/* define short */
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+
+//定数
+#define inf 2147483647;
+#define INF 9223372036854775807;
+
+/* func */
+inline int in_int() {int x; cin >> x; return x;}
+inline ll in_ll() {ll x; cin >> x; return x;}
+inline double in_double() {{double x; cin >> x; return x;}}
+inline string in_str() {string x; cin >> x; return x;}
+inline int ctoi(char c) {return c - '0';}
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+  ll a, b;
+  cin >> a >> b;
+  ll ans = 1, p = a;
+  rep(i,60) {
+    ll wari = (1LL << i);
+    if ((b/wari) % 2 == 1) {
+      ans = (ans*p) % 1000000007;
+    }
+    p = p*p % 1000000007;
+  }
+  cout << ans << endl;
+}
