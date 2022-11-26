228140e747e08c284fc8cb81cc0c9bed98b7cd99
Glan_Sheep <kopkcgungun@gmail.com>
2022-11-26 20:13:34 +0900

    277-c

--- ABC/-280/277/c.cpp
+++ ABC/-280/277/c.cpp
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// clang-format off
-/* accelration */
-// 高速バイナリ生成
-#pragma GCC target("avx")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
-// cin cout の結びつけ解除, stdioと同期しない(入出力非同期化)
-// cとstdの入出力を混在させるとバグるので注意
-struct Fast {Fast() {std::cin.tie(0); ios::sync_with_stdio(false);}} fast;
-
 /* alias */
 // type
 using ull = unsigned long long;
@@ -29,12 +19,8 @@ using vs = vector<string>;
 using vpii = vector<pii>;
 
 /* define short */
-#define pb push_back
 #define mp make_pair
-#define um unordered_map
 #define all(obj) (obj).begin(), (obj).end()
-#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
-#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
 #define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
 
 /* REP macro */
@@ -44,9 +30,9 @@ using vpii = vector<pii>;
 #define repd(i,n) for(ll i=n-1;i>=0;i--)
 #define rrepd(i,n) for(ll i=n;i>=1;i--)
 
-/* debug */
-// 標準エラー出力を含む提出はrejectされる場合もあるので注意
-#define debug(x) cerr << "\033[33m(line:" << __LINE__ << ") " << #x << ": " << x << "\033[m" << endl;
+//定数
+#define inf 2147483647;
+#define INF 9223372036854775807;
 
 /* func */
 inline int in_int() {int x; cin >> x; return x;}
@@ -54,50 +40,32 @@ inline ll in_ll() {ll x; cin >> x; return x;}
 inline double in_double() {{double x; cin >> x; return x;}}
 inline string in_str() {string x; cin >> x; return x;}
 inline int ctoi(char c) {return c - '0';}
-// vector_finder: (arg)elementを vectorの先頭から(arg)search_lengthまで先頭から検索し、boolを返す
-// (arg)search_length: 走査するベクトル長の上限(先頭から何要素目までを検索対象とするか、1始まりで)
-template <typename T> inline bool vector_finder(std::vector<T> vec, T element, unsigned int search_length) {
-  auto itr = std::find(vec.begin(), vec.end(), element);
-  size_t index = std::distance( vec.begin(), itr );
-  if (index == vec.size() || index >= search_length) {return false;} else {return true;}
-}
-template <typename T> inline void print(const vector<T>& v, string s = " ")
-  {rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
-template <typename T, typename S> inline void print(const pair<T, S>& p)
-  {cout << p.first << " " << p.second << endl;}
-template <typename T> inline void print(const T& x) {cout << x << "\n";}
-template <typename T, typename S> inline void print(const vector<pair<T, S>>& v)
-  {for (auto&& p : v) print(p);}
-template <typename T, typename S> inline void print(const map<T, S>& m)
-  {for (auto&& p : m) print(p);}
-// 第一引数と第二引数を比較し、第一引数(a)をより大きい/小さい値に上書き
-template <typename T> inline bool chmin(T& a, const T& b) {bool compare = a > b; if (a > b) a = b; return compare;}
-template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b; if (a < b) a = b; return compare;}
-// gcd lcm
-// clang-format on
-vector<bool> seen;
-int ans = 1;
-void dfs(const vvi &G, int v) {
-  seen[v] = true;
-  for (auto next_v : G[v]) {
-    if (seen[next_v]) continue;
-    dfs(G, next_v);
-    ans = max(next_v, ans);
-  }
-}
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
 int main() {
   int n;
   cin >> n;
-  vi a(n), b(n);
-  vvi G(n*2, vi(n*2));
+  map<int, vi> graph;
   rep(i,n) {
     int a, b;
     cin >> a >> b;
-    G[a].push_back(b);
-    G[b].push_back(a);
+    graph[a].push_back(b);
+    graph[b].push_back(a);
   }
-  seen.assign(n, false);
-  dfs(G, 1);
-  cout << ans << endl;
+  queue<int> que;
+  que.push(1);
+  set<int> s;
+  s.insert(1);
+  while (!que.empty()) {
+    int v = que.front();
+    que.pop();
+    for (int i : graph[v]) {
+      if (!s.count(i)) {
+        que.push(i);
+        s.insert(i);
+      }
+    }
+  }
+  cout << *s.rbegin() << endl;
 }
