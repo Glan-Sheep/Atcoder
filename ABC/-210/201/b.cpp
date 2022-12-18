64cae8a0a164c32851e4919db17189ea9a5476f9
Glan_Sheep <kopkcgungun@gmail.com>
2022-12-18 20:57:55 +0900

    201-b

--- ABC/-210/201/b.cpp
+++ ABC/-210/201/b.cpp
@@ -1,19 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/* alias */
+// type
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+// pair
+using pii = pair<int, int>;
+// vector
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using vpii = vector<pii>;
+
+/* define short */
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+
+//定数
+#define inf 2147483647;
+#define INF 9223372036854775807;
+
+/* func */
+inline int in_int() {int x; cin >> x; return x;}
+inline ll in_ll() {ll x; cin >> x; return x;}
+inline double in_double() {{double x; cin >> x; return x;}}
+inline string in_str() {string x; cin >> x; return x;}
+inline int ctoi(char c) {return c - '0';}
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
 int main() {
-  int N;
-  cin >> N;
-  vector<string> S(N);
-  vector<int> T(N);
-  for (int i = 0; i < N;i++) {
-    cin >> S[i] >> T[i];
+  int n;
+  cin >> n;
+  vector<pair<int, string> > s;
+  rep(i,n) {
+    int a;
+    string b;
+    cin >> b >> a;
+    s.push_back(pair(a,b));
   }
-  vector<int> U = T;
-  sort(T.begin(), T.end(),greater<int>());
-  for (int i = 0; i < N; i++) {
-    if (T[1] == U[i]) {
-      cout << S[i] << endl;
-    }
-  }
+
+  sort(all(s));
+  cout << s[s.size()-2].second << endl;
 }
